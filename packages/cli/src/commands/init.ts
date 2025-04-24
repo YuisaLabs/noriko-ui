26b2ea5c8a25ff2bc40dfffc531f42ae32d1e62f
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2025-04-24 20:36:28 +0700

    fix: button component no empty object type

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,58 +1,16 @@
 import chalk from "chalk";
-import ora from "ora";
-import path from "path";
-import inquirer from "inquirer";
-import fs from "fs-extra";
-import { configFiles, hasPackage, REQUIRED_PACKAGES } from "../utils/packages";
-import { BASE_URL } from "../utils/config";
-import { fetchComponent } from "../utils/fetch";
-
-function getTargetPath(fileName: string): string {
-  if (fileName === "main.css") {
-    return path.join(process.cwd(), "src", fileName);
-  }
-
-  if (fileName === "classes.tsx") {
-    return path.join(process.cwd(), "src", "utils", fileName);
-  }
-
-  return path.join(process.cwd(), fileName);
-}
+import {
+  installPackages,
+  installConfigFiles,
+  installComponents,
+} from "../utils/packages";
 
 async function init() {
   console.log(chalk.cyan("Initializing project..."));
 
-  for (const file of configFiles) {
-    const fileName = path.basename(file.path);
-    const targetPath = getTargetPath(fileName);
-
-    if (fs.existsSync(targetPath)) {
-      const { overwrite } = await inquirer.prompt([
-        {
-          type: "confirm",
-          name: "overwrite",
-          message: `File ${path.basename(targetPath)} already exists. Do you want to overwrite it?`,
-          default: false,
-        },
-      ]);
-
-      if (!overwrite) {
-        console.log(chalk.gray(`Skipping ${targetPath}`));
-        continue;
-      }
-    }
-
-    const spinner = ora(`Downloading ${path.basename(file.path)}`).start();
-    const fileUrl = `${BASE_URL}${file.path}`;
-
-    try {
-      await fetchComponent(fileUrl, targetPath);
-      spinner.succeed(`File ${path.basename(file.path)} downloaded successfully.`);
-    } catch (error) {
-      spinner.fail(`Failed to download ${path.basename(file.path)}`);
-      console.error(chalk.red(`Failed to download ${path.basename(file.path)}`));
-    }
-  }
+  await installPackages();
+  await installConfigFiles();
+  await installComponents();
 
   console.log(chalk.green("Initialized successfully."));
 }
--- packages/cli/src/utils/packages.ts
+++ packages/cli/src/utils/packages.ts
@@ -2,7 +2,11 @@ import path from "path";
 import fs from "fs-extra";
 import chalk from "chalk";
 import ora from "ora";
+import inquirer from "inquirer";
 import { execa } from "execa";
+import { fetchComponent } from "./fetch";
+import { BASE_URL } from "./config";
+import { components } from "./components";
 
 interface ConfigFile {
   path: string;
@@ -20,7 +24,7 @@ const configFiles: ConfigFile[] = [
   },
   {
     path: "/packages/ui/src/utils/classes.tsx",
-  }
+  },
 ];
 
 const REQUIRED_PACKAGES = [
@@ -69,4 +73,134 @@ async function installPackages() {
   }
 }
 
-export { REQUIRED_PACKAGES, hasPackage, configFiles, installPackages };
+function getTargetPath(fileName: string): string {
+  if (fileName === "main.css") {
+    return path.join(process.cwd(), "src", fileName);
+  }
+
+  if (fileName === "classes.tsx") {
+    return path.join(process.cwd(), "src", "utils", fileName);
+  }
+
+  return path.join(process.cwd(), fileName);
+}
+async function installConfigFiles() {
+  const { installConfigFiles } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "installConfigFiles",
+      message: "Do you want to install config files?",
+      default: true,
+    },
+  ]);
+
+  if (!installConfigFiles) {
+    console.log(chalk.gray("Skipping config files installation."));
+    return;
+  }
+
+  for (const file of configFiles) {
+    const fileName = path.basename(file.path);
+    const targetPath = getTargetPath(fileName);
+
+    if (fs.existsSync(targetPath)) {
+      const { overwrite } = await inquirer.prompt([
+        {
+          type: "confirm",
+          name: "overwrite",
+          message: `File ${path.basename(
+            targetPath
+          )} already exists. Do you want to overwrite it?`,
+          default: false,
+        },
+      ]);
+
+      if (!overwrite) {
+        console.log(chalk.gray(`Skipping ${targetPath}`));
+        continue;
+      }
+    }
+
+    const spinner = ora(`Downloading ${path.basename(file.path)}`).start();
+    const fileUrl = `${BASE_URL}${file.path}`;
+
+    try {
+      await fetchComponent(fileUrl, targetPath);
+      spinner.succeed(
+        `File ${path.basename(file.path)} downloaded successfully.`
+      );
+    } catch (error) {
+      spinner.fail(`Failed to download ${path.basename(file.path)}`);
+      console.error(
+        chalk.red(`Failed to download ${path.basename(file.path)}`)
+      );
+    }
+  }
+}
+
+async function installComponents() {
+  if (components.length === 0) {
+    console.log(chalk.yellow("No components to install."));
+    return;
+  }
+
+  const { installBaseComponents } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "installBaseComponents",
+      message: "Do you want to install base components?",
+      default: true,
+    }
+  ]);
+
+  if (!installBaseComponents) {
+    console.log(chalk.gray("Skipping base components installation."));
+    return;
+  }
+
+  console.log(chalk.cyan("Installing base components:"));
+
+  for (const component of components) {
+    const destinationDir = path.join(process.cwd(), "src", "components", "ui", `${component.name}.tsx`);
+
+    await fs.ensureDir(path.dirname(destinationDir));
+
+    if (fs.existsSync(destinationDir)) {
+      const { overwrite } = await inquirer.prompt([
+        {
+          type: "confirm",
+          name: "overwrite",
+          message: `Component ${component.name} already exists.`,
+          default: false,
+        }
+      ]);
+
+      if (!overwrite) {
+        console.log(chalk.gray(`Skipping ${destinationDir}`));
+        continue;
+      }
+    }
+
+    const spinner = ora(`Installing ${component.name} component`).start();
+    const fileUrl = `${BASE_URL}${component.path}`;
+
+    try {
+      await fetchComponent(fileUrl, destinationDir);
+      spinner.succeed(`Component ${component.name} installed successfully.`);
+    } catch (error) {
+      spinner.fail(`Failed to install ${component.name} component`);
+      console.error(chalk.red(`Failed to install ${component.name} component`));
+    }
+  }
+
+  console.log(chalk.green("All base components installed successfully."));
+}
+
+export {
+  REQUIRED_PACKAGES,
+  hasPackage,
+  configFiles,
+  installPackages,
+  installConfigFiles,
+  installComponents,
+};
